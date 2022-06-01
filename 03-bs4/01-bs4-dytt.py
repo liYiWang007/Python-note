c7c02d378b098ddeb954f95fd1473016b6c2c631
liYiWang007 <429521830@qq.com>
2022-06-01 17:09:11 +0800

    bs4抓取静态数据页（例子电影天堂）

--- 03-bs4/01-bs4-dytt.py
+++ 03-bs4/01-bs4-dytt.py
@@ -1,32 +1,27 @@
 import requests
 # 导入bs4
-from bs4 import BeautifulSoup, BeautifulStoneSoup
+from bs4 import BeautifulSoup
+import csv
 
 url='http://www.dytt89.com/'
 resp=requests.get(url)
 resp.encoding='gb2312'
 
+f=open('电影名字.csv',mode='w',encoding='utf-8')
+csv_file=csv.writer(f)
 # 解析数据
 # 1.把页面源代码交给BeautifulSoup处理
 # html.parser 告诉处理器这就是html(不加也不影响程序运行)
 page=BeautifulSoup(resp.text,"html.parser")
 content_div=page.find_all('div',attrs={'class':'co_content222'})[1:] #从第二个开始抓取
+# ul=content_div.find('ul')
+# li=ul.find_all('li')
 for it in content_div:
     uls=it.find('ul')
-    # print(uls)
-
-    
-    for itt in uls:
-        lis=itt.find_all('li')
-        print(lis)
-
-
-
-
-# lis=ul.find_all('li')[3]
-# print(lis)
-
-# ul=content_div.find('ul')
-# for itt in lis:
-#     fonts=itt.find_all('font')
-#     print(fonts[0].text)
+    lis=uls.find_all('li')[2]
+    name=lis.find('a').text
+    date=lis.find('span').text
+    # print(date)
+    csv_file.writerow([name,date])
+f.close()
+print("over")
