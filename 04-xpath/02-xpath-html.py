fd4ef62e85d06a9d15ec7bb829373f71baf7789c
liYiWang007 <429521830@qq.com>
2022-06-07 17:03:20 +0800

    mhl+shl最新评价

--- 04-xpath/02-xpath-html.py
+++ 04-xpath/02-xpath-html.py
@@ -5,5 +5,13 @@ tree=etree.parse('./02-xpath.html')
 # result=tree.xpath('/html/body/ul/li[1]/a/text()') 
 
 #a[@属性='xxx']只获取href包含 bing 字符串的a
-result=tree.xpath('/html/body/ul/li/a[@id="bing"]/text()') 
-print(result)
+# result=tree.xpath('/html/body/ul/li/a[@id="bing"]/text()') 
+
+# 标签/@属性值 ：直接a抓取的href属性值
+# result=tree.xpath('/html/body/ul/li/a/@href')  
+# print(tree.xpath('/html/body/ul/li/a/@href'))
+
+li_list=tree.xpath('/html/body/ul/li') 
+for li in li_list:
+    result=li.xpath('./a/@href')
+    print(result)
--- 04-xpath/mhl/mhl499.py
+++ 04-xpath/mhl/mhl499.py
@@ -2,5 +2,8 @@ from lxml import etree
 parser = etree.HTMLParser(encoding='utf-8')
 tree=etree.parse('./mhl0-499.html',parser=parser)
 
-result=tree.xpath('/html/body//div[@id="comments"]/div[@class="comment-item "]//span[@class="allstar10 rating"]/text()')
-print(result)
+stars_list=tree.xpath('/html/body//div[@id="comments"]/div[@class="comment-item "]')
+for star in stars_list:
+    result=star.xpath('//span[@class="allstar10 rating"]/text()')
+    print(result)
+
