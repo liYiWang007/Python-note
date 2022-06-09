31687cbf93de4a067531aae80e3fc42eddba8d5f
liYiWang007 <429521830@qq.com>
2022-06-09 13:55:47 +0800

    多线程

--- 07-thread/01-threading.py
+++ 07-thread/01-threading.py
@@ -4,23 +4,24 @@
 #     for i in range(4):
 #         print('func',i)
 
+# print(__name__) #-> __main__
 # if __name__=='__main__':
 #     func()
 #     for i in range(3):
 #         print('hello',i)
 
-from threading import Thread
+# from threading import Thread
 
-def func():
-    for i in range(5):
-        print('func',i)
+# def func():
+#     for i in range(5):
+#         print('func',i)
 
 
-if __name__=='__main__':
-    t=Thread(target=func) #创建线程并给线程安排任务
-    t.start() #多线程状态可以开始工作，具体执行时间有CPU决定
-    for i in range(3):
-        print('hello',i)
+# if __name__=='__main__':
+#     t=Thread(target=func) #创建线程并给线程安排任务
+#     t.start() #多线程状态可以开始工作，具体执行时间有CPU决定
+#     for i in range(3):
+#         print('hello',i)
 
 class MyThread(Thread):
     def run(self):
@@ -30,8 +31,8 @@ class MyThread(Thread):
 
 if __name__=='__main__':
     t=MyThread()
-    # t.run() # 用.run() 会变单线程
-    t.start()
+    # t.run() # .run() 是单线程
+    t.start() #开启多线程
 
     for i in range(5):
         print('方法2',i)
